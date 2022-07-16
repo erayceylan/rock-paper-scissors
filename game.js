d7d722b9096e229f0f317c7db6ac5e5496e9c55b
erayceylan <erayceylan@rocketmail.com>
2022-07-16 14:35:29 +0300

    Complete the game UI

--- game.js
+++ game.js
@@ -1,40 +1,67 @@
-const game = { choice: false, playerChoice: "", computerChoice: "", player: 0, computer: 0};
-const panel = document.querySelector(".panel"); panel.innerHTML = "";
+const game = { winner:"", over: false, playerChoice: "", computerChoice: "", player: 0, computer: 0};
+const start = document.querySelector(".start");
+const panel = document.querySelector(".panel");
 const animals = document.querySelectorAll(".animals > *");
-const scoreboard = document.querySelector(".scoreboard"); scoreboard.style.visibility = "hidden";
-const winner = document.querySelector(".winner"); winner.style.visibility = "hidden";
+const scoreboard = document.querySelector(".scoreboard");
+const newgame = document.querySelector(".newgame");
+const audio = document.querySelector("audio");
+const music = document.querySelector(".music");
+
+music.addEventListener('click',playMusic);
+newgame.addEventListener("click",newGame);
 
 newGame();
 
 function newGame() {
+    gameStart();
     panel.innerHTML = 'Welcome to game of "Bee-Fish-Bear", press START to play...';
-    const start = document.querySelector(".start");
     start.addEventListener('click', playGame, {once: true});
 }
 
 function playGame() {
+    roundStart();
+    start.innerHTML = "next round"
     panel.innerHTML = "Pick your animal<br>...<br>";
     animals.forEach(animal => animal.addEventListener('click',playRound));
+
 }
 
-
 function playRound(e) {
-    e.stopPropagation()
-    if (game.playerChoice === "") {
-        let animal = this.className;
-        game.playerChoice = animal;
+    console.log("playRound")
 
+    e.stopPropagation()
+
+    if (game.playerChoice === "") {
+
+        game.playerChoice = this.className;
         game.computerChoice = computerPlay();
 
-        panel.innerHTML += `You chose ${animal}<br>`;
+        panel.innerHTML += `You chose ${game.playerChoice}<br>`;
         panel.innerHTML += `Computer chose ${game.computerChoice}<br>`;
 
-        let resut = decideRoundWinner(game.computerChoice, game.playerChoice);
-        announceRoundResult(resut);
+        announceRoundResult(decideRoundWinner(game.computerChoice, game.playerChoice));
         updateScoreboard();
 
-        game.choice = true;
+        animals.forEach(animal => animal.removeEventListener('click', playRound))
+    }
 
+    if (game.computer == 5 || game.player == 5) {
+        
+        if (game.computer == 5) {panel.innerHTML = "<br>COMPUTER WON!"; game.winner ="computer"}
+        if (game.player == 5) {panel.innerHTML = "<br>YOU WON!"; game.winner = "player"}
+
+        panel.style.fontSize = "36px"
+
+        game.over = true
+
+    }
+    
+    if (game.over) {
+        start.innerHTML = "game over"
+        start.addEventListener('click',gameOver, {once:true})
+    }
+    else {
+        start.addEventListener('click',playGame, {once:true})
     }
 }
 
@@ -81,4 +108,38 @@ function computerPlay() {
     else if (number<=66) pick = "fish"
     else pick = "bear"
     return pick
+}
+
+function roundStart() {
+    game.playerChoice = "";
+    game.computerChoice = "";
+}
+
+function gameStart() {
+    start.innerHTML = "start";
+    panel.innerHTML = "";
+    panel.style.fontSize = "18px";
+    scoreboard.style.visibility = "hidden";
+    game.winner = ""
+    game.over = false;
+    game.playerChoice = "";
+    game.computerChoice = "";
+    game.player = 0;
+    game.computer = 0;
+}
+
+function gameOver() {
+    if (game.winner == "computer") panel.innerHTML = "<br>GG EZ"
+    if (game.winner == "player") panel.innerHTML = "<br>GG WP"
+}
+
+function playMusic(e) {
+    if (e.target.innerHTML == "music on") {
+        audio.play()
+        e.target.innerHTML = "music off"
+    }
+    else {
+        audio.pause()
+        e.target.innerHTML = "music on"
+    }
 }
